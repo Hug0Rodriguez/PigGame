a16105fe97b83793b34475a69406556f70acd8e4
Hugo <76200652+Hug0Rodriguez@users.noreply.github.com>
2023-12-21 07:50:45 -0500

    added ability to finish game
    CSS for winning the game is not working properly will figure it out tommorow

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // Selecting elements
+const player0EL = document.querySelector(".player--0");
+const player1EL = document.querySelector(".player--1");
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.getElementById("score--1");
 const currrent0EL = document.getElementById("current--0");
@@ -15,9 +17,19 @@ score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add("hidden");
 
+// Holdiing player scores
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle("player--active");
+    player1EL.classList.toggle("player--active");
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -34,6 +46,29 @@ btnRoll.addEventListener("click", function () {
             currentScore;
     } else {
         // switch to next player
-        activePlayer = activePlayer === 0 ? 1 : 0;
+        switchPlayer();
     }
+});
+
+btnHold.addEventListener("click", function () {
+    console.log([activePlayer]);
+    // 1. add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+        scores[activePlayer];
+
+    // 2. check if player's score is >= 100
+    if (scores[activePlayer] >= 20) {
+        playing = false;
+        document
+            .querySelector(`.player--${activePlayer}`)
+            .classList.add("winner");
+        document
+            .querySelector(`.player--${activePlayer}`)
+            .classList.remove("player--active");
+    } else {
+        switchPlayer();
+    }
+    // finish the game
+    // switch to the next player
 });
